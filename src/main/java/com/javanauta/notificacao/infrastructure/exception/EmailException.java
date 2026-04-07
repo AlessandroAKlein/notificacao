7a21b8759ee9f2b2abad09c7502bda970ace4c62
Alessandro <Alessandro.klein13@yahoo.com.br>
2026-04-06 22:48:25 -0300

    adicionado método de enviar email

---
+++ src/main/java/com/javanauta/notificacao/infrastructure/exception/EmailException.java
@@ -0,0 +1,12 @@
+package com.javanauta.notificacao.infrastructure.exception;
+
+public class EmailException extends RuntimeException {
+    public EmailException(String message) {
+
+        super(message);
+    }
+
+    public EmailException (String message, Throwable throwable){
+        super(message,throwable);
+    }
+}
